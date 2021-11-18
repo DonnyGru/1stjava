6ce49c0eacab095d8475a15bdb511c2e927dc70a
DonnyGru <91632259+DonnyGru@users.noreply.github.com>
2021-11-18 20:19:46 +0100

    Add files via upload

---
+++ 1st_Javascript_Playground/script.js
@@ -0,0 +1,20 @@
+console.log("Hello Winc Academy");
+let name = 'Donny'
+console.log(name);
+//Met if is geen variabele mogelijk om te maken//
+var sum=4+4;
+console.log("Addition result:",sum);
+var y="4"+"4";
+console.log(y);
+var z=sum-4;
+console.log("Subtraction result:",z);
+var l=3*7;
+console.log("Multiplication result:",l);
+var k=l/8;
+console.log("Division result:",k);
+var j=l%8;
+console.log("Modulus result:",j);
+var age=28;
+console.log(typeof age);
+var age=("28");
+console.log(typeof age);
